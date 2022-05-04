eb042b7983590f61f9f1ad3c100312839e557e31
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-04 07:21:31 +0530

    Update terraform/tf-module-alb/

---
+++ listener.tf
@@ -0,0 +1,17 @@
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 0 : 1
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
+  }
+}
+
